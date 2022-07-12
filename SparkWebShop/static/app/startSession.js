b9a45aa813400fbba816ca5a9a67d7efa864f9e8
mihajlo-ra92 <maksimovic.ra92.2019@uns.ac.rs>
2022-07-12 03:48:11 +0200

    Made it so that buyer can see only open objects when logging workout.

--- SparkWebShop/static/app/startSession.js
+++ SparkWebShop/static/app/startSession.js
@@ -27,7 +27,8 @@ Vue.component("start-session", {
 						<th scope="col">Grade</th>
 					</tr>
 						
-					<tr v-for="so in sportObjects" v-on:click="selectObject(so)">
+					<tr v-for="so in sportObjects" v-if="so.isOpen" v-on:click="selectObject(so)">
+						
 						<td scope="row">{{so.name }}</td>
 						<td>{{so.objectType }}</td>
 						<td v-if="so.isOpen">Open</td>
